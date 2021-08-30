a009e33a21392e4edbeb6bdddbff081f42af3243
AnushkaVaid <89016525+AnushkaVaid@users.noreply.github.com>
2021-08-30 15:35:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+body{
+  text-align:center;
+  background: linear-gradient(#B0BEC5 30%,#00838F 80%);
+  background-repeat:no-repeat;
+}
+
+img{
+  height:350px;
+  
+}
+
+h1{
+  text-shadow:2px 2px #212121;
+  color:#006064;
+
+}
